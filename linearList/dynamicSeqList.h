57227875902624fba0c1909e5e705456a6e90a65
missingyouzi <623659328@qq.com>
2019-08-02 13:57:36 +0800

    add dynamicSeqList.h

--- linearList/dynamicSeqList.h
+++ linearList/dynamicSeqList.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define initSize 5
 #define increaseSize 1
 
@@ -8,25 +9,44 @@ typedef struct
     int* data;
     int listSize, length;
 }DSeqList, *PDSeqList;
+
 void initList(PDSeqList pdsl){
     pdsl->data = (int*)malloc(sizeof(int) * initSize);
     pdsl->listSize = initSize;
     pdsl->length = 0;
+    memset(pdsl->data, 0, sizeof(pdsl->data));
 }
 void destroyList(PDSeqList pdsl){
     free(pdsl->data);
     pdsl->length = 0;
+    pdsl->data = NULL;
+    pdsl->listSize = 0;
 }
-void printList(PDSeqList pdsl){
-    for (int i = 0;i < pdsl->length;i ++)
-        printf("%d ", pdsl->data[i]);
+void printList(DSeqList dsl){
+    for (int i = 0;i < dsl.length;i ++)
+        printf("%d ", dsl.data[i]);
     printf("\n");
 }
 void insertList(PDSeqList pdsl, int pos, int ele){
-    
+    if (pos < 1 || pos > pdsl->listSize+1)
+        exit(0);
+    if (pdsl->length >= pdsl->listSize){
+        pdsl->listSize += increaseSize;
+        pdsl->data = (int*)realloc(pdsl->data, sizeof(int)*pdsl->listSize);
+    }
+    for (int i = pdsl->length-1;i >= pos-1;i --)
+        pdsl->data[i+1] = pdsl->data[i];
+    pdsl->data[pos-1] = ele;
+    pdsl->length ++;
 }
 int deleteList(PDSeqList pdsl, int pos){
-    
+    if (pos < 1 || pos > pdsl->length)
+        exit(0);
+    int a = pdsl->data[pos-1];
+    for (int i = pos;i < pdsl->length;i ++) // 1 2 3, 1
+        pdsl->data[i-1] = pdsl->data[i];
+    pdsl->length --;
+    return a;
 }
 int searchList(DSeqList dsl, int ele){
     for (int i = 0;i < dsl.length;i ++){ // 1 2 3 4, 2
--- main.c
+++ main.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
-#include "linearList/staticSeqList.h"
+#include "linearList/dynamicSeqList.h"
 
 int main(int argc, char const *argv[])
 {
-    SSeqList ssl;
-    initList(&ssl);
-    insertList(&ssl, 1, 1);
-    insertList(&ssl, 2, 2);
-    insertList(&ssl, 3, 3);
-    printList(&ssl);
+    // SSeqList ssl;
+    // initList(&ssl);
+    // insertList(&ssl, 1, 1);
+    // insertList(&ssl, 2, 2);
+    // insertList(&ssl, 3, 3);
+    // printList(&ssl);
 
-    printf("%d\n", deleteList(&ssl, 2));
-    printList(&ssl);
-    printf("%d\n", searchList(ssl, 3));
+    // printf("%d\n", deleteList(&ssl, 2));
+    // printList(&ssl);
+    // printf("%d\n", searchList(ssl, 3));
+    DSeqList dsl;
+    initList(&dsl);
+    insertList(&dsl, 1, 1);
+    insertList(&dsl, 2, 2);
+    insertList(&dsl, 3, 3);
+    insertList(&dsl, 4, 4);
+    insertList(&dsl, 5, 5);
+    insertList(&dsl, 6, 6);// check more than initSize
+    printList(dsl);
+    printf("%d\n", deleteList(&dsl, 2));
+    printList(dsl);
+    printf("%d\n", searchList(dsl, 4));
     return 0;
 }
